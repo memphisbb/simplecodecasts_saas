a42cb52c0c5ed4835d0c1dafb6d319d49c89d093
Brad Brown <memphis.brad.brown@gmail.com>
2015-12-05 18:17:44 +0000

    removed :enable_startstls_auto => true

--- config/environment.rb
+++ config/environment.rb
@@ -8,9 +8,10 @@ Rails.application.initialize!
 ActionMailer::Base.smtp_settings = {
     :address => 'smtp.sendgrid.net',
     :port => '587',
-    :authentcation => :plain,
+    :authentication => :plain,
     :user_name => ENV['SENDGRID_USERNAME'],
     :password => ENV['SENDGRID_PASSWORD'],
     :domain => 'heroku.com'
-    :enable_startstls_auto => true
+    
+    
 }
